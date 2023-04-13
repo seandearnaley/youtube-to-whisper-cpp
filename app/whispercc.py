c62736f61e5e4e9928cd022204e9659d921b6295
Sean Dearnaley <SeanDearnaley@hotmail.com>
2023-04-12 23:30:13 -0400

    Update whispercc.py

--- app/whispercc.py
+++ app/whispercc.py
@@ -1,11 +1,10 @@
 """WhisperCC binding module"""
-from __future__ import annotations
-
+import time
 from pathlib import Path
 
 from whispercpp import Whisper
 
-from .audio_tools import NdArray, convert_to_float_array, decode_audio
+from app.audio_tools import NdArray, convert_to_float_array, decode_audio
 
 
 class WhisperTranscriber:
@@ -18,7 +17,16 @@ class WhisperTranscriber:
     def transcribe_audio(self, audio_file: Path) -> str:
         """Transcribe audio from a file"""
         audio_data = self._load_audio(audio_file)
+        start_time = time.time()
         transcription = self.whisper.transcribe(audio_data)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        hours, remainder = divmod(execution_time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        print(
+            "Transcription elapsed execution time:"
+            f" {int(hours)}:{int(minutes):02d}:{seconds:06.3f}"
+        )
         return transcription
 
     def _load_audio(self, audio_file: Path) -> NdArray:
